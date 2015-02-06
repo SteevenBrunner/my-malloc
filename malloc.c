d16ccfdb9c7a776ba1be5646f6e95b4b436db38f
brunne_s <brunne_s@epitech.eu>
2015-02-06 11:32:09 +0100

    Malloc avec sbrk de la structure

--- malloc.c
+++ malloc.c
@@ -5,7 +5,7 @@
 ** Login   <brunne_s@epitech.net>
 ** 
 ** Started on  Mon Feb  2 17:03:00 2015 Steeven Brunner
-** Last update Wed Feb  4 15:40:57 2015 Steeven Brunner
+** Last update Fri Feb  6 11:31:41 2015 Steeven Brunner
 */
 
 #include <sys/types.h>
@@ -13,39 +13,53 @@
 #include <stdio.h>
 //#include <stdlib.h>		//include malloc / free / realloc
 
-//#include "malloc.h"
+#include "malloc.h"
 
 // sbrk(X) pour allouer plus
 // sbrk(0) pour connaitre la limite actuellement alouée (break)
 
-void	*malloc(size_t size)
+void		*malloc(size_t size)
 {
-  void *p; 
-  p = sbrk(0);
-  printf("size = %d\n", size);
-  //If sbrk fails , we return NULL
-  printf("1 : sbk(0) = %x\n", sbrk(0));
-  if (sbrk(size) == (void*) - 1)
-    return  NULL;
-  printf("2 : sbk(0) = %x\n", sbrk(0));
-  return  p;
+  t_block	*block;
+
+  block = sbrk(0);
+  printf("old_break = %x\n", block);
+  if (sbrk(BLOCK_SIZE) == (void*) - 1)
+    {
+      printf("[Error] : sbrk failed\n");
+      return (NULL);
+    }
+  printf("BLOCK_SIZE = %d\n", BLOCK_SIZE);
+  printf("new_break = %x\n", sbrk(0));
+  return (block);
 }
 
-t_block	add_blocks(t_block my_block, size_t size)
+/*t_block		find_block(t_block *last, size_t size)
+{
+  t_block	b = base;
+  while (b && !(b->free && b->size >= size)) 
+    {
+      *last = b;
+      b = b->next;
+    }
+  return (b);
+}
+
+t_block		add_blocks(t_block my_block, size_t size) //extend_heap
 {
   t_block	new_block;
 
   new_block = sbrk(0);
   if(sbrk(size + STRUCT_SIZE) == (void*) - 1)
     {
-      printf("Error : sbrk\n");
+      printf("Error : sbrk() failed\n");
       return (NULL);
     }
   if (my_block)
     my_block->next = new_block;
   new_block->free = 0;
   return (new_block);
-}
+  }*/
 
 void	create_tab(int	*tab)
 {
@@ -54,7 +68,7 @@ void	create_tab(int	*tab)
 
 int	main()
 {
-  int	*tab2;
+  /*int	*tab2;
   tab2 = malloc(3 * sizeof(int));
   tab2[0] = 3;
   tab2[1] = 4;
@@ -71,8 +85,10 @@ int	main()
   printf("\n-------------\n\n");
   
   create_tab(tab2);
-  create_tab(tab3);
-  
+  create_tab(tab3);*/
+  printf("First step\n");
+  malloc(7 * sizeof(int));
+  printf("Second step\n");
 }
 
   //brk()
--- malloc.h
+++ malloc.h
@@ -5,14 +5,14 @@
 ** Login   <brunne_s@epitech.net>
 ** 
 ** Started on  Tue Feb  3 14:40:50 2015 Steeven Brunner
-** Last update Wed Feb  4 11:22:07 2015 Steeven Brunner
+** Last update Fri Feb  6 11:30:10 2015 Steeven Brunner
 */
 
 typedef	struct	block
 {
   size_t	size;
   int		free;
-  t_block	next;
+  struct block	*next;
 }		t_block;
 
-#define BLOCK_SIZE sizeof(struct s_block)
+#define BLOCK_SIZE sizeof(t_block)
