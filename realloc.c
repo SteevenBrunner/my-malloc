e1327984ee05cc2385979631f57783489b352968
decamp_t <decamp_t@epitech.eu>
2015-02-10 12:33:06 +0100

    realloc v.1

--- realloc.c
+++ realloc.c
@@ -5,10 +5,18 @@
 ** Login   <brunne_s@epitech.net>
 ** 
 ** Started on  Mon Feb  2 17:04:06 2015 Steeven Brunner
-** Last update Mon Feb  2 17:04:29 2015 Steeven Brunner
+** Last update Tue Feb 10 12:31:35 2015 Thomas Decamp
 */
 
-void	*realloc(void *ptr, size_t size)
-{
+#include <stdlib.h>
+#include <unistd.h>
 
+void	*my_realloc(void *ptr, size_t size)
+{
+  void	*copy;
+
+  copy = malloc(size * sizeof(ptr));
+  copy = &ptr;
+  //free(ptr);
+  return (copy);
 }
