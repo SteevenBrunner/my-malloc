fbd883b82f549e91645912c349fdcb60a9dbb18a
decamp_t <decamp_t@epitech.eu>
2015-02-11 15:40:03 +0100

    calloc done (correction)

--- realloc.c
+++ realloc.c
@@ -5,18 +5,18 @@
 ** Login   <brunne_s@epitech.net>
 ** 
 ** Started on  Mon Feb  2 17:04:06 2015 Steeven Brunner
-** Last update Tue Feb 10 12:31:35 2015 Thomas Decamp
+** Last update Wed Feb 11 15:05:44 2015 Thomas Decamp
 */
 
 #include <stdlib.h>
 #include <unistd.h>
 
-void	*my_realloc(void *ptr, size_t size)
+void	*realloc(void *ptr, size_t size)
 {
   void	*copy;
 
   copy = malloc(size * sizeof(ptr));
   copy = &ptr;
-  //free(ptr);
+  free(ptr);
   return (copy);
 }
