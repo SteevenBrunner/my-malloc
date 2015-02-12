ff7683ae966943f2e1e445e8d6597cd818844f0c
decamp_t <decamp_t@epitech.eu>
2015-02-12 10:39:09 +0100

    Mise en commentaire des prototypes re/calloc dans .h

--- malloc.h
+++ malloc.h
@@ -5,7 +5,7 @@
 ** Login   <brunne_s@epitech.net>
 ** 
 ** Started on  Tue Feb  3 14:40:50 2015 Steeven Brunner
-** Last update Thu Feb 12 10:36:08 2015 Thomas Decamp
+** Last update Thu Feb 12 10:38:13 2015 Thomas Decamp
 */
 
 #ifndef MALLOC_H_
@@ -20,8 +20,8 @@ typedef	struct	block
 
 void	*malloc(size_t size);
 void	free(void *ptr);
-void	*realloc(void *ptr, size_t size);
-void	*calloc(size_t nmeb, size_t size);
+//void	*realloc(void *ptr, size_t size);
+//void	*calloc(size_t nmeb, size_t size);
 
 extern t_block	*g_root;
 
